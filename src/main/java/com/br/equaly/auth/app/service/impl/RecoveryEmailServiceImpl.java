5d60e110d08ea1df3734ad72a68dfc0871e2172c
RafaelRenaud <renaudrafa@hotmail.com>
2023-10-08 23:15:27 -0300

    fix: Refatoração de Código

--- src/main/java/com/br/equaly/auth/app/service/impl/RecoveryEmailServiceImpl.java
+++ src/main/java/com/br/equaly/auth/app/service/impl/RecoveryEmailServiceImpl.java
@@ -4,8 +4,8 @@ import com.br.equaly.auth.app.model.dto.recovery.RecoveryEmailRequestDTO;
 import com.br.equaly.auth.app.service.RecoveryEmailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +17,14 @@ public class RecoveryEmailServiceImpl implements RecoveryEmailService {
     @Autowired
     private JmsTemplate jmsTemplate;
 
-    @Autowired
-    private Queue queue;
+    @Value("${spring.jms.email.queue-name}")
+    private String queueName;
 
     @Override
     public void sendRecoveryEmail(RecoveryEmailRequestDTO recoveryEmailRequestDTO) throws IOException {
         ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
         jmsTemplate.convertAndSend(
-                this.queue.getName(),
+                queueName,
                 objectWriter.writeValueAsBytes(recoveryEmailRequestDTO)
         );
     }
